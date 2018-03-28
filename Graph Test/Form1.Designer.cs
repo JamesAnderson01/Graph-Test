28ff85900d89a6ed2c90aa2a375a0651a9a67134
RC0030808 <RC0030808@CRO-ICF07-09>
2018-03-28 13:57:56 +0100

    Add project files.

---
+++ Graph Test/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Graph_Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.yXToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.yX2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.yX3ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(0, 39);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(523, 419);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.yXToolStripMenuItem,
+            this.yX2ToolStripMenuItem,
+            this.yX3ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(981, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // yXToolStripMenuItem
+            // 
+            this.yXToolStripMenuItem.Name = "yXToolStripMenuItem";
+            this.yXToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.yXToolStripMenuItem.Text = "y = x";
+            this.yXToolStripMenuItem.Click += new System.EventHandler(this.yXToolStripMenuItem_Click);
+            // 
+            // yX2ToolStripMenuItem
+            // 
+            this.yX2ToolStripMenuItem.Name = "yX2ToolStripMenuItem";
+            this.yX2ToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.yX2ToolStripMenuItem.Text = "y = x2";
+            // 
+            // yX3ToolStripMenuItem
+            // 
+            this.yX3ToolStripMenuItem.Name = "yX3ToolStripMenuItem";
+            this.yX3ToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.yX3ToolStripMenuItem.Text = "y = x3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(981, 604);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem yXToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem yX2ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem yX3ToolStripMenuItem;
+    }
+}
+
